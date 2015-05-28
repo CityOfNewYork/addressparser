7572fc5e61106572d7aaf00dd59692ca38ca201b
Amal Sudama <cds.sudama@gmail.com>
2015-05-28 15:48:43 -0400

    modify historic regex pattern to replace - (dash)

--- historicMappings.py
+++ historicMappings.py
@@ -2,39 +2,43 @@
 
 import re
 queens = [
-    '(Jacksons+Heights)\s+Historic\s+District',
-    '(Douglaston)\s+Historic\s+District'
+    '-(Jackson Heights)\s+Historic\s+District',
+    '-(Douglaston)\s+Historic\s+District'
 ]
 queens = [re.compile(i, re.IGNORECASE) for i in queens]
 
 brooklyn = [
-    '(Fillmore\s+Place)\s+Historic\s+District',
-    '(Fulton\s+Ferry)\s+Historic\s+District',
-    '(Fort\s+Greene)\s+Historic\s+District',
-    '(Boerum Hill)\s+Historic\s+District',
-    '(Brooklyn Heights)\s+Historic\s+District',
-    '(Cobble Hill)\s+Historic\s+District',
-    '(Crown Heights)\s+North\s+Historic\s+District\s+II'
+    '-(Fillmore\s+Place)\s+Historic\s+District',
+    '-(Fulton\s+Ferry)\s+Historic\s+District',
+    '-(Fort\s+Greene)\s+Historic\s+District',
+    '-(Boerum Hill)\s+Historic\s+District',
+    '-(Brooklyn Heights)\s+Historic\s+District',
+    '-(Cobble Hill)\s+Historic\s+District',
+    '-(Crown Heights)\s+North\s+Historic\s+District\s+II'
 ]
 brooklyn = [re.compile(i, re.IGNORECASE) for i in brooklyn]
 
 manhattan = [
-    '(Building)-Individual\s+Landmark',
-    '(Tribeca)\s+West\s+Historic\s+District',
-    '(SoHo)Cast\s+Iron\s+Historic\s+District',
-    '(NoHo)\s+Historic\s+District'
+    '-(Building)-Individual\s+Landmark',
+    '-(Tribeca)\s+West\s+Historic\s+District',
+    '-(SoHo)Cast\s+Iron\s+Historic\s+District',
+    '-(NoHo)\s+Historic\s+District'
 ]
 manhattan = [re.compile(i, re.IGNORECASE) for i in manhattan]
 
 
 def preprocess(text):
+    orig = text
     for rex in queens:
-        text = rex.sub('\\1 Queens, NY', text)
+        text = rex.sub(', \\1, Queens, NY.\n', text)
 
     for rex in brooklyn:
-        text = rex.sub('\\1 Brooklyn, NY', text)
+        text = rex.sub(', \\1, Brooklyn, NY.\n', text)
 
     for rex in manhattan:
-        text = rex.sub('\\1 NY, NY', text)
-
+        text = rex.sub(', \\1, NY, NY.\n', text)
+    if orig != text:
+        print 'Original: [%s]' % orig
+        print 'Processed: [%s]' % text
+        print '===' * 12
     return text
