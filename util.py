dcf26f2674868ee6b4c4aace635651adfa292a64
Amal Sudama <cds.sudama@gmail.com>
2015-05-28 15:49:41 -0400

    add regex to filter block address

--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import re
 import historicMappings
 
@@ -9,18 +10,31 @@ _rex_manhattan = re.compile('(in\s+the\s+)Borough\s+of\s+'
                             '(Manhattan)',
                             re.IGNORECASE)
 
+# match these...
+# BOROUGH OF QUEENS 15-5446-Block 1289, lot 15–
+# BOROUGH OF QUEENS 15-7412 - Block 8020, lot 6–
+# BOROUGH OF BROOKLYN 15-7494-Block 2382, lot 3–
+# BOROUGH OF MANHATTAN 15-6223 – Block 15, lot 22-
+_b = '[brooklyn|bronx|manhattan|staten\s+island|queens]'
+_rex_blockcodes = r'BOROUGH\s+of\s+%s[^b]+block[^,]+,\s+lot[\s\d]+.' % _b
+_rex_blockcodes = re.compile(_rex_blockcodes, re.IGNORECASE)
 
 def filter_boroughs(text):
     global _rex_boroughs, _rex_manhattan
-    text = _rex_manhattan.sub('NY, NY', text)
-    return _rex_boroughs.sub('\\2, NY', text)
+    text = _rex_manhattan.sub('NY, NY.\n', text)
+    return _rex_boroughs.sub('\\2, NY.\n', text)
 
+
+def filter_blockcodes(text):
+    global _rex_blockcodes
+    return '.\n'.join( [para for para in _rex_blockcodes.split(text)])
 
 def preproces_text(text):
     # replace unicode dash(-) with ascii dash(-)
     text = text.replace(u'\u2013', '-')
 
     text = filter_boroughs(text)
+    text = filter_blockcodes(text)
     return historicMappings.preprocess(text)
 
 
