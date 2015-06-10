c4b6381a080409c4016999cb276e966e6dc0fa99
C Amal Sudama <cds.sudama@gmail.com>
2015-06-10 03:14:49 +0000

    test against 20014-10015 list of NYC schools

---
+++ tests/testHighSchoolAddresses.py
@@ -0,0 +1,45 @@
+import sys
+sys.path.append('..')
+
+from nose.plugins.skip import SkipTest
+from nose.plugins.attrib import attr
+import os.path
+import unittest
+import codecs
+import re
+
+from nyctext import nycaddress as parser
+
+class HighSchools(unittest.TestCase):
+
+    def __init__(self, *args, **kwds):
+        super(HighSchools, self).__init__(*args, **kwds)
+        self.cwd = os.path.dirname(__file__)
+        # extract list of addresses
+        text = open(os.path.join(self.cwd, 'data', 'highschools.txt')).readlines()
+        self.reg_ny = re.compile('New York, NY', re.I)
+        self.addresses = [self.normalize(l.split(':')[-1].strip()) for l in text]
+
+
+
+    def normalize(self, ad):
+        return self.reg_ny.sub('Manhattan, NY', ad)
+        
+
+    def checkExpectation(self, source, verbose=False):
+        addresses = parser.parse(source, verbose)
+        for loc in addresses:
+            self.assertIn(loc, self.addresses)
+            self.addresses.remove(loc)
+
+        print 'Address not discovered'
+        for ad in self.addresses:
+            print ad
+        self.assertEqual(self.addresses, [])
+
+
+    @attr(type="wip")
+    def testFoo(self):
+        source =  open(os.path.join(self.cwd, 'data', 'highschools.txt')).read().decode('latin1')
+        self.checkExpectation(source)
+
