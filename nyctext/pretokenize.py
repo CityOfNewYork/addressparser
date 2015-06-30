b4ed2863ba8f6860eda29127631667bab1eab5e5
Amal Sudama <cds.sudama@gmail.com>
2015-06-29 20:21:26 -0400

    add suite of pretokenizing processing

--- nyctext/pretokenize.py
+++ nyctext/pretokenize.py
@@ -11,6 +11,55 @@ together.
 _rex_initials = re.compile('(?<=[\s\.][A-Z])(\.)', re.I)
 
 _rex_reverend = re.compile('rev\.', re.I)
+_rex_suite = re.compile('ste\.', re.I)
+
+_street_abbreviations = re.compile('\s+(str?\.?)[\s,]', re.IGNORECASE)
+_avenue_abbreviations = re.compile('\s+(ave?\.?)[\s,]', re.IGNORECASE)
+_boulevard_abbreviations = re.compile('\s+(blvd?\.?)[\s,]', re.IGNORECASE)
+_plaza_abbreviations = re.compile('\s+(plz?\.?)[\s,]', re.IGNORECASE)
+_drive_abbreviations = re.compile('\s+(dr?\.?)[\s,]', re.IGNORECASE)
+_parkway_abbreviations = re.compile('\s+(pkwy?\.?)[\s,]', re.IGNORECASE)
+_road_abbreviations = re.compile('\s+(rd\.?)[\s,]', re.IGNORECASE)
+_lane_abbreviations = re.compile('\s+(ln\.?)[\s,]', re.IGNORECASE)
+_expressway_abbreviations = re.compile('\s+(expy\.?)[\s,]', re.IGNORECASE)
+_highway_abbreviations = re.compile('\s+(hwy\.?)[\s,]', re.IGNORECASE)
+_circle_abbreviations = re.compile('\s+(cir\.?)[\s,]', re.IGNORECASE)
+
+
+def filter_street_abbreviations(text):
+    # Todo: Build a more comprehensive list of throughways.
+    # See: http://www.semaphorecorp.com/cgi/abbrev.html
+
+    global _street_abbreviations, _avenue_abbreviations
+    global _boulevard_abbreviations, _plaza_abbreviations, _drive_abbreviations
+    global _parkway_abbreviations, _road_abbreviations, _lane_abbreviations
+
+    text = _street_abbreviations.sub(' Street ', text)
+    text = _avenue_abbreviations.sub(' Avenue ', text)
+    text = _boulevard_abbreviations.sub(' Boulevard ', text)
+    text = _plaza_abbreviations.sub(' Plaza ', text)
+    text = _drive_abbreviations.sub(' Drive ', text)
+    text = _parkway_abbreviations.sub(' Parkway ', text)
+    text = _road_abbreviations.sub(' Road ', text)
+    text = _lane_abbreviations.sub(' Lane ', text)
+    text = _expressway_abbreviations.sub(' Expressway ', text)
+    text = _highway_abbreviations.sub(' Highway ', text)
+    text = _circle_abbreviations.sub(' Circle ', text)
+    return text
+
+
+_rex_cd = re.compile(r'\s?(\d+)(n|s|e|w)(\.)?\s', re.I)
+
+# 110E 21st -> 100 E 21 st
+# 110E 22nd
+# 110E 23rd
+# 110E 24th
+# 110E 25th.
+
+
+def filter_cd(text):
+    global _rex_cd
+    return _rex_cd.sub('\\1 \\2 ', text)
 
 
 def do_initials(text):
@@ -21,6 +70,11 @@ def do_initials(text):
 def do_title(text):
     global _rex_reverend
     return _rex_reverend.sub('Rev', text)
+
+
+def do_suite(text):
+    global _rex_suite
+    return _rex_suite.sub('Suite', text)
 
 
 def transform(text, verbose=False):
@@ -34,5 +88,17 @@ def transform(text, verbose=False):
     text = do_title(text)
     if verbose:
         print '   titlesub: %s' % text
+
+    text = do_suite(text)
+    if verbose:
+        print '   suitesub: %s' % text
+
+    text = filter_street_abbreviations(text)
+    if verbose:
+        print 'street abbr: %s' % text
+
+    text = filter_cd(text)
+    if verbose:
+        print '    cd abbr: %s' % text
 
     return text
