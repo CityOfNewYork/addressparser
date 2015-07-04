9980c5defc34eb125ba5f2b39f0d9a67e6490269
Amal Sudama <cds.sudama@gmail.com>
2015-07-04 12:52:44 -0400

    add regex conditional for n s e w

--- nyctext/neighborhoods/regexps.py
+++ nyctext/neighborhoods/regexps.py
@@ -10,7 +10,7 @@ def make_neighorbood_regex(lHoods, city):
     # Don't match if neighborhood is followed
     # by a thoroughfare name
     names = throughway_names[1:-1]  # remove parens
-    names = '(%s|%s)' % (names, city)
+    names = '(%s|%s|north|south|east|west)' % (names, city)
 
     rex = '\\s(%s)(?![\s,]*(%s))' % (hoods, names)
 
