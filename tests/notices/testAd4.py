2274049ca3b61d72e215e7534123a2f6f16da66d
Amal Sudama <cds.sudama@gmail.com>
2015-07-08 10:37:55 +0000

    trim dead code

--- tests/notices/testAd4.py
+++ tests/notices/testAd4.py
@@ -1,5 +1,4 @@
-from nose.plugins.skip import SkipTest
-from nose.plugins.attrib import attr
+import pytest
 from ..expectations import ParseExpectationsFromFile
 
 
@@ -8,7 +7,6 @@ class LandmarkPreservation(ParseExpectationsFromFile):
     def __init__(self, *args, **kwds):
         super(LandmarkPreservation, self).__init__(*args, **kwds)
 
-    @attr(test='wip')
     def testLandmarkPreservation(self):
         'landmark preservation sample'
         self.checkExpectation('ad-sample4.txt', 'ad-expected4.txt')
